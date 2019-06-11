321e5a8edb61d4a387823bca1b3073a664aefc8c
NourSerw <nourserw@hotmail.com>
2019-06-11 18:58:10 +0200

    Final submission.

--- Configuration_files/trackman_data_challenge.py
+++ Configuration_files/trackman_data_challenge.py
@@ -5,62 +5,41 @@ File done by Nour Al-Rahman Al-Serw for Trackman's code challenge on the 11th of
 import json
 import sys
 import re
-import string
 import os.path
 
 stack_array = []
 depth = 0
 ascii_tree = ''
+query_file = str(sys.argv[1])
 
 
 # Main function
 def main():
     global stack_array
-    # At the moment I'm using a hardcoded json file to test the functionality
-    query_file = "games.nulls"  # name of said file
-    # stack_array.append(query_file)
-    stack_array.append((query_file, depth))
-    # if check_if_exists(query_file):
-    #     get_table_dependency(query_file)
+    stack_array.append((query_file, 0))
     iterate_req()
 
 
-
-# Heavy work function to retieve the depencies
+#Traversers through the json files and json tags to find the dependencies
 def get_table_dependency(query_tuple):
-    global stack_array
-    global depth
+    global stack_array #To disallow the creation of a local variable with the same name
     file_name = query_tuple[0]+'.json'
 
-    if (os.path.exists(file_name)):
-        depth = depth + 1
+    if (os.path.exists(file_name)): #If loop to find if said file exists
         with open(file_name) as json_file:
-
             data = json.load(json_file)
             table_names = data['query']['L'][0]['M']['from']['S']
             # table_names = table_names.split(table_names," on ")[]
-            table_names = table_names.split(" on ", 1)[0]
+            table_names = table_names.split(" on ", 1)[0] #Remove text characters after the word on
             array_of_from_query = re.split("\\s", table_names)
             for el in array_of_from_query:
                 if "." in el:
                     el = el + ".json"
-                    # if (check_if_exists(el)) :
-                    #     stack_array.append(el.replace('.json', ''))
-                    # else:
-                    #     stack_array.pop(el)
+                    newDepth = query_tuple[1] + 1 #Set the depth for the table(s)
+                    stack_array.append((el.replace('.json', ''), newDepth))
+                    # I used a stack as a data structure to store the tables and dependent tables with elements
+                    # full of tuples. Each tuple has two elements: the table and the depth
 
-                    # stack_array.append(el.replace('.json', ''))
-                    stack_array.append((el.replace('.json', ''), depth))
-
-
-        # if check_if_exists(stack_array[-1] + ".json"):
-        #     get_table_dependency(stack_array[-1] + ".json")
-        # print(stack_array)
-
-    else:
-        print("leaf node")
-        depth = depth - 1
-        # print(depth)
 
 
 
@@ -68,12 +47,19 @@ def get_table_dependency(query_tuple):
 def iterate_req():
     global ascii_tree
     while(len(stack_array) != 0):
-        element = stack_array.pop()
 
-        print("===================")
-        print(element)
-        print("===================")
+        element = stack_array.pop()
+        element_string = ''.join(element[0])
+        if element[1] == 0:
+            ascii_tree = element_string + '\n'
+        else:
+            ascii_tree += '\t' * element[1] + '|' + '\n'
+            ascii_tree += '\t' * element[1] + '|' + '+' + element_string + '\n'
+
         get_table_dependency(element)
 
 
 main()
+file = open(query_file + ".txt", "w") #Write file to .txt file
+file.write(ascii_tree)
+file.close()
--- README.md
+++ README.md
@@ -11,3 +11,10 @@ The approach taken has been divided into three parts which are:
 
 Language used: Python 3.7
 
+Instructions on how to use:
+	1. Make sure the script is in the same directory as the .json files.
+	2. Navigate to the folder containing both the .json files and the script
+	3. Run the python file and pass in the argument with it as well. So for example it will look something like this 
+		"python trackman_data_challenge.py games.nulls"
+
+Then a .txt file will appear on the same directory with the ascii written inside of it.
